b2894757be0336e405556fe8a199e8590fae2932
rogerdepaulaa <roger.philippe.18@gmail.com>
2022-08-24 23:01:03 -0300

    tolltip

---
+++ js/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function initTooltip() {
+  const tooltip = document.querySelectorAll('[data-tooltip');
+
+  tooltip.forEach((item) => {
+    item.addEventListener('mouseover', onMouseOver);
+  })
+
+  
+  function onMouseOver(event){
+    const tooltipBox = criarTooltipBox(this);
+
+    onMouseMove.tooltipBox = tooltipBox;
+    this.addEventListener('mousemove', onMouseMove)
+
+    onMouseLeave.tooltipBox = tooltipBox;
+    onMouseLeave.element = this;
+    this.addEventListener('mouseleave', onMouseLeave);
+  }
+
+  const onMouseLeave= {
+    handleEvent(){
+      this.tooltipBox.remove();
+      this.element.removeElementListener('mouseleave', onMouseLeave)
+      this.element.removeElementListener('mousemove', onMouseMove)
+    }
+  }
+
+  const onMouseMove ={
+    handleEvent(event){      
+    this.tooltipBox.style.top = event.pageY + 15 + 'px';
+    this.tooltipBox.style.left = event.pageX + 15 +'px';
+    }
+  }
+    
+  function criarTooltipBox(element){
+    const tooltipBox = document.createElement('div');
+    const text = element.getAttribute('aria-label');
+    tooltipBox.classList.add('tooltip');
+    tooltipBox.innerText = text;
+    document.body.appendChild(tooltipBox);
+    return tooltipBox;
+
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-import initScrollSuave from './modules/scroll-suave.js'
-import initAnimacaoScroll from  './modules/animacao-scroll.js'
-import initAccordion from './modules/accordion.js'
-import initTabNav from './modules/nav.js'
-import initModal from  './modules/modal.js'
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimacaoScroll from  './modules/animacao-scroll.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/nav.js';
+import initModal from  './modules/modal.js';
+import initTooltip from  './modules/tooltip.js';
+
+
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
+initTooltip();
